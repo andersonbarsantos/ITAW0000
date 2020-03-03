ce9b4f8db528ac8e031907a65874f7ea3d3610c5
Anderson Santos <anderson.santos@confitec.com.br>
2020-03-03 17:35:42 -0300

    03-03-2020

--- ITAW000/Scripts/sidebar.js
+++ ITAW000/Scripts/sidebar.js
@@ -2,6 +2,25 @@
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
+
+    $('#dataTable').DataTable();
 }
+
+$(function () {
+    $('#dataTable').DataTable({
+        "pageLength": 3,
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false
+    });
+});
  
-$(document).ready(function () { init(); });
+$(document).ready(function () {
+    init();
+
+
+
+});
