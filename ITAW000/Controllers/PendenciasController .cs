0277eb133205e8fb646954db9fc685ccfd5be6d0
Anderson Santos <anderson.santos@confitec.com.br>
2020-03-04 19:25:00 -0300

    19:24

---
+++ ITAW000/Controllers/PendenciasController .cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ITAW000.Models;
+using ITAW000.Repository;
+
+namespace ITAW000.Controllers
+{
+    public class PendenciasController : Controller
+    {
+        private RegraRepository regraRespository = new RegraRepository();
+        // GET: Regra
+        public ActionResult Index()
+        {
+            return View(regraRespository.GetAll());
+        }
+
+        // GET: Regra/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Regra/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Regra/Create
+        [HttpPost]
+        public ActionResult Edit(RegraViewModel objModel)
+        {
+            try
+            {            
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+
+    }
+}
