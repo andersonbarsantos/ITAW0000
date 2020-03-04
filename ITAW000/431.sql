13e5d68406847a4b4dd3219c00942e53b55d1a5b
Anderson Barreto <andersonbarsantos@gmail.com>
2020-03-04 08:05:41 -0300

    sql

---
+++ ITAW000/431.sql
@@ -0,0 +1,85 @@
+
+drop table regra_tb
+go
+create table regra_tb (
+IdRegra int identity ,
+idResponsavel int,
+idSistema int,
+idTipo int,
+idSituacao int,
+idRetorno int,
+Ativo varchar(15) ,
+Descricao varchar(255) ,
+Quantidade int,
+DtInicioVigencia DateTime ,
+DtFimVigencia DateTime ,
+DtInclusao DateTime,
+DtAlteracao DateTime,
+Usuario varchar(255) 
+) 
+
+drop table responsavel_tb
+go
+create table responsavel_tb (
+IdResponsavel int identity ,
+Nome varchar(255) ,
+Descricao varchar(255) ,
+DtInclusao DateTime,
+DtAlteracao DateTime,
+Usuario varchar(255) 
+) 
+
+drop table retorno_tb
+go
+create table retorno_tb (
+IdRetorno int identity ,
+Nome varchar(255) ,
+Descricao varchar(255) ,
+DtInclusao DateTime,
+DtAlteracao DateTime,
+Usuario varchar(255) 
+) 
+
+drop table Sistema_tb
+go
+create table Sistema_tb (
+IdSistema int identity ,
+Nome varchar(255) ,
+Descricao varchar(255) ,
+DtInclusao DateTime,
+DtAlteracao DateTime,
+Usuario varchar(255) 
+) 
+
+
+drop table Situacao_tb
+go
+create table Situacao_tb (
+IdSituacao int identity ,
+Nome varchar(255) ,
+Descricao varchar(255) ,
+DtInclusao DateTime,
+DtAlteracao DateTime,
+Usuario varchar(255) 
+) 
+
+drop table Tipo_tb
+go
+create table Tipo_tb (
+IdTipo int identity ,
+Nome varchar(255) ,
+Descricao varchar(255) ,
+DtInclusao DateTime,
+DtAlteracao DateTime,
+Usuario varchar(255) 
+) 
+
+
+select * from regra_tb
+select * from responsavel_tb
+select * from retorno_tb
+select * from Sistema_tb
+select * from Situacao_tb
+select * from Tipo_tb
+
+
