0b6d0324a9d776a3922d7451b58b3ba819688380
Anderson Barreto <andersonbarsantos@gmail.com>
2020-03-08 19:55:04 -0300

    19:54

--- ITAW000/App_Start/BundleConfig.cs
+++ ITAW000/App_Start/BundleConfig.cs
@@ -20,13 +20,12 @@ namespace ITAW000
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/sidebar.js"
+                      "~/Scripts/bootstrap.js"
                       ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"
+                      ));
         }
     }
 }
